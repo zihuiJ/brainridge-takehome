1fe6ad3fa1a403f6c4ab56e4dd31c50ea7a926c4
zihuiJ <z1201jiang@gmail.com>
2024-11-05 13:32:45 -0500

    set up transfer funds

--- src/app/transfer-funds/transfer-funds.component.ts
+++ src/app/transfer-funds/transfer-funds.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-transfer-funds',
-  standalone: true,
-  imports: [],
   templateUrl: './transfer-funds.component.html',
-  styleUrl: './transfer-funds.component.css'
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
 })
 export class TransferFundsComponent {
+  transferForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.transferForm = this.fb.group({
+      fromAccount: ['', Validators.required],
+      toAccount: ['', Validators.required],
+      amount: [0, [Validators.required, Validators.min(1)]],
+    });
+  }
 }
