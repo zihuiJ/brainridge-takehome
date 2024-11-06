0b505d1cd6140151b2e0e86dc668e8f24209bc77
zihuiJ <z1201jiang@gmail.com>
2024-11-05 22:43:55 -0500

    refactor create account ui

--- src/app/shared/create-account-button/create-account-button.component.ts
+++ src/app/shared/create-account-button/create-account-button.component.ts
@@ -3,11 +3,16 @@ import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-create-account-button',
-  template: `<button mat-raised-button [disabled]="!isAccountSelected">Create Account</button>`,
+  template: `
+    <button mat-raised-button [disabled]="!isAccountSelected">
+      Create {{ accountType ? accountType : 'Account' }}
+    </button>
+  `,
   standalone: true,
   imports: [MatButtonModule]
 })
 export class CreateAccountButtonComponent {
   @Input() isAccountSelected: boolean = false;
+  @Input() accountType: string = '';
 }
 
